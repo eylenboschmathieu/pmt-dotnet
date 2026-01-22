912443bf0b7d6e6dc716c5e12b403455dc2d5779
Mathieu <mathieu.eylenbosch@gmail.com>
2026-01-22 13:26:05 +0100

    Database migration

---
+++ PMT.Data/Migrations/20260122122547_Changed_UserShift_Field_Name.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PMT.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Changed_UserShift_Field_Name : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Confirmed",
+                table: "UserShifts",
+                newName: "Planned");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Planned",
+                table: "UserShifts",
+                newName: "Confirmed");
+        }
+    }
+}
